29eeca4e7ea71749db9f4031d970bbc201d425c3
pranavakula <117644551+pranavakula@users.noreply.github.com>
2024-10-23 11:53:13 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as new version
+
+// alert(3+3) we are using node.js not browser(only in browser)
+let name="pranav"
+let age= 19
+let isLoggedin= false
+
+//number 2^53
+//bigint 
+//string
+//boolean
+//null => standalone value and a data type too
+//undefined
+//symbol=> unique
+
+console.log(typeof name);
+console.log(typeof null)
+console.log(typeof undefined)
+
